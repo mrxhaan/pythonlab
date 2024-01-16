195f8cc2a5b3c0fdaada5382ac2cdd6e3db2d15e
mrxhaan <0xhaan@gmail.com>
2024-01-16 03:50:18 +0000

    added even counting

---
+++ even_counting.py
@@ -0,0 +1,13 @@
+# This program will count upto certain number
+
+mynumber=0
+
+# Loop over until 100
+while mynumber <= 100:
+  print(mynumber)
+  # increment mynumber by 2 every loop
+  mynumber=mynumber + 2
+
+
+
+
