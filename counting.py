87eeb01c12948978092987b9eb4017018ba6deb6
mrxhaan <0xhaan@gmail.com>
2024-01-16 02:25:44 +0000

    A program to count numbers

---
+++ counting.py
@@ -0,0 +1,10 @@
+# This program will count upto certain number
+
+mynumber=0
+
+print(mynumber)
+# Loop over until 100
+while mynumber <= 100:
+  print(mynumber)
+  # increment mynumber by 1 every loop
+  mynumber=mynumber + 1
