a0705ee78ad19afb2ea2fa905af9480be5d59dab
mrxhaan <0xhaan@gmail.com>
2024-07-04 19:59:36 +0000

    added multiplication and division

--- mycalc/calculator.py
+++ mycalc/calculator.py
@@ -5,3 +5,17 @@ def addition(num1,num2):
 def subtraction(num1,num2):
    result=(num1 - num2) 
    return result
+
+def multiplication(num1,num2):
+   result=(num1 * num2) 
+   return result
+
+def division(num1,num2):
+   try:
+      result=(num1 / num2) 
+      return result
+   except ZeroDivisionError:
+      print("Second number should be nonzero")
+   except:
+      print("Unexpected error")
+   
--- mycalc/mycalc.py
+++ mycalc/mycalc.py
@@ -2,15 +2,24 @@ import sys
 import calculator
 
 command=sys.argv[1]
-firstnum=int(sys.argv[2])
-secondnum=int(sys.argv[3])
 
-
+try:
+   firstnum=int(sys.argv[2])   
+   secondnum=int(sys.argv[3])
+except ValueError:
+   print("please only enter numbers")
+   sys.exit(1)
 if command == 'addition':
    result=calculator.addition(firstnum,secondnum)
    print(result)
 elif command =='subtraction':
    result=calculator.subtraction(firstnum,secondnum)
    print(result)
+elif command == 'multiplication':
+   result=calculator.multiplication(firstnum,secondnum)
+   print(result)
+elif command == 'division':
+   result=calculator.division(firstnum,secondnum)
+   print(result)
 else:
-   print("invalid operation")
+  print("invalid operation")
